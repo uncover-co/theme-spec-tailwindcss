c4fcd6ecc8709cea76563ea8c07d5f73262946d1
Georges Boris <georgesboris@gmail.com>
2022-07-11 16:24:07 -0300

    theme-spec v5 compat

--- src/index.js
+++ src/index.js
@@ -7,21 +7,20 @@ const plugin = require("tailwindcss/plugin");
 const namespace = "tmspc";
 
 const fontVariables = {
-  title: "font-title",
-  text: "font-text",
-  code: "font-code",
+    title: "font-title",
+    text: "font-text",
+    code: "font-code",
 };
 
-const colorVariants = ["", "-light", "-contrast"];
+const colorVariants = ["-bg", "-fg", "-aux"];
 const colorChannels = "-ch";
 const colorVariables = [
-  "background",
-  "baseline",
-  "primary",
-  "secondary",
-  "success",
-  "warning",
-  "danger",
+    "baseline",
+    "primary",
+    "secondary",
+    "success",
+    "warning",
+    "danger",
 ];
 
 /**
@@ -29,9 +28,9 @@ const colorVariables = [
  */
 
 const fontFamily = {
-  title: `--${namespace}-${fontVariables.title}`,
-  text: `--${namespace}-${fontVariables.text}`,
-  code: `--${namespace}-${fontVariables.code}`,
+    title: `--${namespace}-${fontVariables.title}`,
+    text: `--${namespace}-${fontVariables.text}`,
+    code: `--${namespace}-${fontVariables.code}`,
 };
 
 /**
@@ -41,80 +40,80 @@ const fontFamily = {
 let colors = {};
 
 colorVariables.forEach((variable) => {
-  colorVariants.forEach((variant) => {
-    colors[`${variable}${variant}`] = withOpacityValue(
-      `--${namespace}-${variable}${variant}${colorChannels}`
-    );
-  });
+    colorVariants.forEach((variant) => {
+        colors[`${variable}${variant}`] = withOpacityValue(
+            `--${namespace}-${variable}${variant}${colorChannels}`
+        );
+    });
 });
 
 module.exports = plugin.withOptions(
-  (userOptions) => {
-    const options = userOptions ?? {};
+    (userOptions) => {
+        const options = userOptions ?? {};
 
-    return ({ addBase }) => {
-      if (options.strict) {
-        addBase({
-          body: {
-            background: toVar(`--${namespace}-background`),
-            fontFamily: toVar(fontFamily.text),
-          },
-          h1: {
-            fontFamily: toVar(fontFamily.title),
-          },
-          h2: {
-            fontFamily: toVar(fontFamily.title),
-          },
-          h3: {
-            fontFamily: toVar(fontFamily.title),
-          },
-          h4: {
-            fontFamily: toVar(fontFamily.title),
-          },
-          h5: {
-            fontFamily: toVar(fontFamily.title),
-          },
-          h6: {
-            fontFamily: toVar(fontFamily.title),
-          },
-          code: {
-            fontFamily: toVar(fontFamily.code),
-          },
-        });
-      }
-    };
-  },
-  (userOptions) => {
-    const options = userOptions ?? {};
-    const extraColors = (options.colors || []).reduce((acc, color) => {
-      acc[color] = withOpacityValue(`--${color}`);
-      return acc;
-    }, {});
+        return ({ addBase }) => {
+            if (options.strict) {
+                addBase({
+                    body: {
+                        background: toVar(`--${namespace}-background`),
+                        fontFamily: toVar(fontFamily.text),
+                    },
+                    h1: {
+                        fontFamily: toVar(fontFamily.title),
+                    },
+                    h2: {
+                        fontFamily: toVar(fontFamily.title),
+                    },
+                    h3: {
+                        fontFamily: toVar(fontFamily.title),
+                    },
+                    h4: {
+                        fontFamily: toVar(fontFamily.title),
+                    },
+                    h5: {
+                        fontFamily: toVar(fontFamily.title),
+                    },
+                    h6: {
+                        fontFamily: toVar(fontFamily.title),
+                    },
+                    code: {
+                        fontFamily: toVar(fontFamily.code),
+                    },
+                });
+            }
+        };
+    },
+    (userOptions) => {
+        const options = userOptions ?? {};
+        const extraColors = (options.colors || []).reduce((acc, color) => {
+            acc[color] = withOpacityValue(`--${color}`);
+            return acc;
+        }, {});
 
-    if (options.strict) {
-      return {
-        theme: {
-          fontFamily,
-          colors: {
-            ...extraColors,
-            ...colors,
-          },
-        },
-      };
+        if (options.strict) {
+            return {
+                theme: {
+                    fontFamily,
+                    colors: {
+                        ...extraColors,
+                        ...colors,
+                    },
+                },
+            };
+        }
+
+        return {
+            theme: {
+                extend: {
+                    fontFamily,
+                    colors: {
+                        ...extraColors,
+                        ...colors,
+                    },
+                },
+            },
+        };
     }
-
-    return {
-      theme: {
-        extend: {
-          fontFamily,
-          colors: {
-            ...extraColors,
-            ...colors,
-          },
-        },
-      },
-    };
-  }
 );
 
 /**
@@ -122,14 +121,14 @@ module.exports = plugin.withOptions(
  */
 
 function withOpacityValue(variable) {
-  return ({ opacityValue }) => {
-    if (opacityValue === undefined) {
-      return `rgb(var(${variable}))`;
-    }
-    return `rgb(var(${variable}) / ${opacityValue})`;
-  };
+    return ({ opacityValue }) => {
+        if (opacityValue === undefined) {
+            return `rgb(var(${variable}))`;
+        }
+        return `rgb(var(${variable}) / ${opacityValue})`;
+    };
 }
 
 function toVar(str) {
-  return `var(${str})`;
+    return `var(${str})`;
 }
